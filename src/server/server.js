672b9e6fefd5964f591a3c65343abe21078be5e0
juls27 <89221003@student.upr.si>
2026-08-18 12:52:03 +0200

    simple server works

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
@@ -6,8 +8,74 @@ app.get("/", (req, res) => {
   res.send("Movie cluster server is running");
 });
 
+
+
+app.get("/cluster/single/:genre", (req, res) => {
+
+  const genre = req.params.genre;
+
+  const filePath = path.join(
+    process.cwd(),
+    "data",
+    "processed",
+    "single",
+    `${genre}.json`
+  );
+
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).json({
+      error: "Cluster not found"
+    });
+  }
+
+  const data = fs.readFileSync(
+    filePath,
+    "utf8"
+  );
+
+  const movies = JSON.parse(data);
+
+  res.json(movies);
+});
+
+
+
+
+app.get("/cluster/pair/:pair", (req, res) => {
+
+  const pair = req.params.pair;
+
+  const filePath = path.join(
+    process.cwd(),
+    "data",
+    "processed",
+    "pairs",
+    `${pair}.json`
+  );
+
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).json({
+      error: "Cluster not found"
+    });
+  }
+
+  const data = fs.readFileSync(
+    filePath,
+    "utf8"
+  );
+
+  const movies = JSON.parse(data);
+
+  res.json(movies);
+});
+
+
+//server start
+
 app.listen(PORT, () => {
+
   console.log(
     `Server running on http://localhost:${PORT}`
   );
+
 });
