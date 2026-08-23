581ce3129782bef2f8f4afa553fd349d77de8e05
juls27 <89221003@student.upr.si>
2026-08-23 14:30:44 +0200

    receiving clusters works

---
+++ src/client/client.js
@@ -0,0 +1,86 @@
+//request cluster from server
+
+async function getCluster(type, clusterName) {
+
+  
+  // work correctly inside the URL
+  const encodedName = encodeURIComponent(clusterName);
+
+  const url =
+    `http://localhost:3000/cluster/${type}/${encodedName}`;
+
+  console.log("Requesting cluster:");
+  console.log(clusterName);
+
+  console.log("URL:");
+  console.log(url);
+
+  // Send HTTP request
+  const response = await fetch(url);
+
+  // Check whether server returned an error
+  if (!response.ok) {
+    throw new Error(
+      `Server returned ${response.status}: ${response.statusText}`
+    );
+  }
+
+  // Convert received JSON into JavaScript objects
+  const movies = await response.json();
+
+  return movies;
+}
+
+function createGenrePairs(genres){
+  const pairs = [];
+
+  for (let i=0; i< genres.length; i++){
+    for (let j=i+1; j<genres.length; j++){
+      const pair = [genres[i], genres[j]].sort().join("+");
+      pairs.push(pair);
+
+    }
+  }
+  return pairs;
+}
+
+//main client logic
+async function main() {
+
+  
+
+  //hardcoded for now: 
+  const selectedGenres = ["Drama", "Romance", "Thriller"];
+  console.log("Selected genres", selectedGenres);
+  const selectedPairs = createGenrePairs(selectedGenres);
+  console.log(selectedPairs);
+
+  const requests = selectedPairs.map((pair)=>{
+    return getCluster("pair", pair);
+  })
+
+  console.log(requests.length);
+
+  const clusterResults = await Promise.all(requests);
+
+  console.log(clusterResults.length);
+
+  for (let i = 0; i<selectedPairs.length; i++){
+    console.log(selectedPairs[i], "->", clusterResults[i].length);
+  }
+
+
+  
+}
+
+
+//start client
+
+main().catch((error) => {
+
+  console.error(
+    "Client failed:",
+    error
+  );
+
+});
