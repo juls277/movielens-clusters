1b9b3eae9c19543b81e23128b4c5faa05d5b0bf6
juls27 <89221003@student.upr.si>
2026-09-20 14:20:18 +0200

    server loads bytes of 2 clusters

--- pir-prototype/server/main.go
+++ pir-prototype/server/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	//read the file into a byte slice data
+	data, err := os.ReadFile("two-clusters.bin")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Loaded %d cluster bytes\n", len(data))
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { // Run this function when someone requests /health.
 		fmt.Fprintln(w, "PIR server running")
 	})
